8eebab659cd36bd340a668c8a5f646e5e5288b54
Ben Jones <benpaddlejones@outlook.com>
2023-08-10 14:11:57 +1000

    refine backend code

--- public/index.js
+++
@@ -1,9 +0,0 @@
-const db = sqlite3.Database("db.sqlite", (err) => {
-    if (err) {
-      // Cannot open database
-      console.error(err.message);
-      throw err;
-    } else {
-      console.log("Connected to the SQLite database.");
-    }
-  });
