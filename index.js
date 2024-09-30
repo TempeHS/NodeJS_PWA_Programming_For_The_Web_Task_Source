4a1c034a35e29a8396d0287ca21d4201bec3f97f
Ben Jones <benpaddlejones@outlook.com>
2024-09-30 14:49:45 +1000

    add python implementation

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/* Python Implementation, comment out js implementaion below before running
+
+    const spawn = require("child_process").spawn;
+    // you can add arguments with spawn('python',["path/to/script.py", arg1, arg2, ...])
+    const pythonProcess = spawn('python',["database_manager.py"]);
+
+*/
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('.database/data_source.db');
 
